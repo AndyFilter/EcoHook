361b3e18d65d0c221b87eb2c5c653553f1c2f878
AndyFilter <gmaciejg525@gmail.com>
2023-09-30 21:19:37 +0200

    Major issues fixed and added unhooking 😳

--- ecohook.cpp
+++ ecohook.cpp
@@ -1,6 +1,7 @@
 #include <string>
 #include <iostream>
 #include <string.h>
+#include <map>
 
 #include "ecohook.h"
 
@@ -12,17 +13,36 @@ const bool is_x86 = false;
 const bool is_x86 = true;
 #endif
 
+struct HookedFuncInfo
+{
+    LPVOID newOriginalFuncAddy;
+    unsigned int len;
+};
+
+
+inline bool ReadMem(LPVOID tgAddress, LPVOID bufferAddress, size_t size, SIZE_T* bytesRead = nullptr) {
+    if (Hook::CustomReadMem)
+        return Hook::CustomReadMem(tgAddress, bufferAddress, size, bytesRead);
+    else
+        return ReadProcessMemory(GetCurrentProcess(), tgAddress, bufferAddress, size, bytesRead);
+}
+
+inline bool WriteMem(LPVOID tgAddress, LPVOID bufferAddress, size_t size, SIZE_T* bytesRead = nullptr) {
+    if (Hook::CustomWriteMem)
+        return Hook::CustomWriteMem(tgAddress, bufferAddress, size, bytesRead);
+    else
+        return WriteProcessMemory(GetCurrentProcess(), tgAddress, bufferAddress, size, bytesRead);
+}
+
+// <Original Func addy, new addy used to call the original func>
+std::map<LPVOID, HookedFuncInfo> HookedFuncs;
+
 // Requires at least PROCESS_VM_READ on the funcAddress of size 5
 LPVOID GetRealFunctionAddress(LPVOID funcAddress)
 {
     byte codeBuffer[6]{ 0 };
-    bool succ = false;
-    if (Hook::CustomReadMem)
-        succ = Hook::CustomReadMem(funcAddress, codeBuffer, sizeof(codeBuffer), NULL);
-    else
-        succ = ReadProcessMemory(GetCurrentProcess(), funcAddress, codeBuffer, sizeof(codeBuffer), NULL);
 
-    if (!succ)
+    if (!ReadMem(funcAddress, codeBuffer, sizeof(codeBuffer)))
         return nullptr;
 
     uint32_t funcOffset = 0x0;
@@ -32,11 +52,12 @@ LPVOID GetRealFunctionAddress(LPVOID funcAddress)
     {
         memcpy(&funcOffset, codeBuffer + 1, sizeof(int));
 
-        printf("Func offset is: 0x%04x\n", funcOffset);
-
         uint64_t finalFuncAddy = (funcOffset + (uint64_t)funcAddress + 5);
 
+#ifdef _DEBUG
+        printf("Func offset is: 0x%04x\n", funcOffset);
         printf("End Func at: 0x%p\n", (void*)finalFuncAddy);
+#endif
         //printf("End Func at: 0x%I64X\n", finalFuncAddy);
 
         return (LPVOID)finalFuncAddy;
@@ -70,6 +91,7 @@ int SearchByteArray(byte* src, byte* pattern, size_t srcLen, size_t patternLen, 
     return foundPatterns;
 }
 
+// targetFunc -> func you want to hook to. detourFunc -> func that u want to be called instead. originalFunc -> a new address to call the original function by
 // Argument len has to be at least 5
 bool Hook::HookFunc(LPVOID targetFunc, LPVOID detourFunc, LPVOID* originalFunc, unsigned int len)
 {
@@ -94,7 +116,7 @@ bool Hook::HookFunc(LPVOID targetFunc, LPVOID detourFunc, LPVOID* originalFunc, 
 
 #endif // _DEBUG
 
-    auto realTgAddress = is_x86 ? targetFunc : GetRealFunctionAddress(targetFunc);
+    auto realTgAddress = GetRealFunctionAddress(targetFunc);
 
     if (realTgAddress == nullptr)
         return false;
@@ -111,14 +133,11 @@ bool Hook::HookFunc(LPVOID targetFunc, LPVOID detourFunc, LPVOID* originalFunc, 
 
     BYTE* startInstructions = new BYTE[len];
     ZeroMemory(startInstructions, len);
-    bool readStatus = false;
-    if (CustomReadMem)
-        readStatus = CustomReadMem(realTgAddress, startInstructions, len, NULL);
-    else
-        readStatus = ReadProcessMemory(GetCurrentProcess(), realTgAddress, startInstructions, len, NULL);
 
-    if (!readStatus)
+    if (!ReadMem(realTgAddress, startInstructions, len)) {
+        VirtualProtect((LPVOID)((uint64_t)realTgAddress - (ReadSize / 2)), ReadSize + 1, prot, &tempProt);
         return false;
+    }
 
 #ifdef _DEBUG
     printf("Default instructions: 0x%1X, 0x%1X, 0x%1X, 0x%1X, 0x%1X\n", startInstructions[0], startInstructions[1], startInstructions[2], startInstructions[3], startInstructions[4]);
@@ -149,17 +168,12 @@ bool Hook::HookFunc(LPVOID targetFunc, LPVOID detourFunc, LPVOID* originalFunc, 
 
         BYTE spaceBufferDown[ChunkSize]{ 0 };
         SIZE_T bytesReadDown = 0;
-        if (CustomReadMem)
-            readStatus = CustomReadMem((void*)((uint64_t)realTgAddress + positiveByteOffset), spaceBufferDown, ChunkSize, &bytesReadDown);
-        else
-        ReadProcessMemory(GetCurrentProcess(), (void*)((uint64_t)realTgAddress + positiveByteOffset), spaceBufferDown, ChunkSize, &bytesReadDown);
+
+        ReadMem((void*)((uint64_t)realTgAddress + positiveByteOffset), spaceBufferDown, ChunkSize, &bytesReadDown);
 
         BYTE spaceBufferUp[ChunkSize]{ 0 };
         SIZE_T bytesReadUp = 0;
-        if (CustomReadMem)
-            readStatus = CustomReadMem((void*)((uint64_t)realTgAddress - (positiveByteOffset + ChunkSize)), spaceBufferUp, ChunkSize, &bytesReadUp);
-        else
-        ReadProcessMemory(GetCurrentProcess(), (void*)((uint64_t)realTgAddress - (positiveByteOffset + ChunkSize)), spaceBufferUp, ChunkSize, &bytesReadUp);
+        ReadMem((void*)((uint64_t)realTgAddress - (positiveByteOffset + ChunkSize)), spaceBufferUp, ChunkSize, &bytesReadUp);
 
         // find empty space for detour
         if (possibleSpaces < detourMaxSpaces)
@@ -210,7 +224,7 @@ bool Hook::HookFunc(LPVOID targetFunc, LPVOID detourFunc, LPVOID* originalFunc, 
         }
 
         if (originalStartSpaceAddy == 0)
-            goto CleanUp; // Exit Should return false
+            goto CleanUpFalse; // Exit Should return false
 
 #ifdef _DEBUG
         printf("Selected Original Space Addy: 0x%p\n", originalStartSpaceAddy);
@@ -224,11 +238,7 @@ bool Hook::HookFunc(LPVOID targetFunc, LPVOID detourFunc, LPVOID* originalFunc, 
         auto toHookAddy = (uint64_t)detourFunc;
         memcpy(detour_shell_code + 6, &toHookAddy, sizeof(void*));
 
-        if (CustomWriteMem)
-            CustomWriteMem(detourSpaceAddy, detourSpaceAddy, sizeof(detour_shell_code), NULL);
-        else
-            WriteProcessMemory(GetCurrentProcess(), detourSpaceAddy, detour_shell_code, sizeof(detour_shell_code), NULL);
-
+        WriteMem(detourSpaceAddy, detour_shell_code, sizeof(detour_shell_code));
 
         BYTE* original_shell_code = new BYTE[trampolinePatternSize];
         memset(original_shell_code, 0x90, trampolinePatternSize);
@@ -236,29 +246,25 @@ bool Hook::HookFunc(LPVOID targetFunc, LPVOID detourFunc, LPVOID* originalFunc, 
 
         memcpy(original_shell_code, startInstructions, len);
 
-        int hookedJmpRelativeAddy = ((uint64_t)realTgAddress) - (uint64_t)originalStartSpaceAddy - len;
+        int hookedJmpRelativeAddy = ((uint64_t)realTgAddress) - (uint64_t)originalStartSpaceAddy - 5;
         memcpy(original_shell_code + len + 1, &hookedJmpRelativeAddy, sizeof(int));
         // original_shell_code should now be equal to original {len} bytes of the target function + 0xE9 (JMP) + relative address (from the current addy)
         // of the continuation of the target function (realTgAddress + len). You call this addy when u want to call the original function as a whole
 
 #ifdef _DEBUG
-        printf("jmp Relative addy : 0x%4X\n", hookedJmpRelativeAddy);
+        printf("jmp Relative addy: 0x%04X\n", hookedJmpRelativeAddy);
 #endif
 
-        if (CustomWriteMem)
-            CustomWriteMem(originalStartSpaceAddy, original_shell_code, trampolinePatternSize, NULL);
-        else
-            WriteProcessMemory(GetCurrentProcess(), originalStartSpaceAddy, original_shell_code, trampolinePatternSize, NULL);
-
+        WriteMem(originalStartSpaceAddy, original_shell_code, trampolinePatternSize);
 
         BYTE* trampoline_shell_code = new BYTE[len]; // relative JMP to the code that jumps to our function 
         memset(trampoline_shell_code, 0x90, len); // fill it with 0x90 (NOP) - no operation, so that if the just is shorter than len, it will just do nothing.
         trampoline_shell_code[0] = 0xE9;
 
-        int relativeAddress = (uint64_t)detourSpaceAddy - ((uint64_t)realTgAddress) - len;
+        int relativeAddress = (uint64_t)detourSpaceAddy - ((uint64_t)realTgAddress) - 5;
 
 #ifdef _DEBUG
-        printf("Relative addy : 0x%4X\n", relativeAddress);
+        printf("Relative addy : 0x%04X\n", relativeAddress);
 #endif
 
         memcpy(trampoline_shell_code + 1, &relativeAddress, sizeof(int));
@@ -267,10 +273,7 @@ bool Hook::HookFunc(LPVOID targetFunc, LPVOID detourFunc, LPVOID* originalFunc, 
         printf("Trampoline hook: 0x%1X, 0x%1X, 0x%1X, 0x%1X, 0x%1X\n", trampoline_shell_code[0], trampoline_shell_code[1], trampoline_shell_code[2], trampoline_shell_code[3], trampoline_shell_code[4]);
 #endif
 
-        if (CustomWriteMem)
-            CustomWriteMem(realTgAddress, trampoline_shell_code, len, NULL);
-        else
-            WriteProcessMemory(GetCurrentProcess(), realTgAddress, trampoline_shell_code, len, NULL);
+        WriteMem(realTgAddress, trampoline_shell_code, len);
 
 
         *originalFunc = originalStartSpaceAddy;
@@ -289,13 +292,10 @@ bool Hook::HookFunc(LPVOID targetFunc, LPVOID detourFunc, LPVOID* originalFunc, 
         memcpy(original_shell_code + len + 1, &hookedJmpRelativeAddy, sizeof(int));
 
 #ifdef _DEBUG
-        printf("jmp Relative addy : 0x%4X\n", hookedJmpRelativeAddy);
+        printf("jmp Relative addy : 0x%04X\n", hookedJmpRelativeAddy);
 #endif
 
-        if (CustomWriteMem)
-            CustomWriteMem(originalStartSpaceAddy, original_shell_code, trampolinePatternSize, NULL);
-        else
-            WriteProcessMemory(GetCurrentProcess(), originalStartSpaceAddy, original_shell_code, trampolinePatternSize, NULL);
+        WriteMem(originalStartSpaceAddy, original_shell_code, trampolinePatternSize);
 
 
         BYTE* trampoline_shell_code = new BYTE[len];
@@ -305,7 +305,7 @@ bool Hook::HookFunc(LPVOID targetFunc, LPVOID detourFunc, LPVOID* originalFunc, 
         int relativeAddress = (uint64_t)detourFunc - ((uint64_t)realTgAddress) - 5;
 
 #ifdef _DEBUG
-        printf("Relative addy : 0x%4X\n", relativeAddress);
+        printf("Relative addy : 0x%04X\n", relativeAddress);
 #endif
 
         memcpy(trampoline_shell_code + 1, &relativeAddress, sizeof(int));
@@ -314,10 +314,7 @@ bool Hook::HookFunc(LPVOID targetFunc, LPVOID detourFunc, LPVOID* originalFunc, 
         printf("Trampoline hook: 0x%1X, 0x%1X, 0x%1X, 0x%1X, 0x%1X\n", trampoline_shell_code[0], trampoline_shell_code[1], trampoline_shell_code[2], trampoline_shell_code[3], trampoline_shell_code[4]);
 #endif
 
-        if (CustomWriteMem)
-            CustomWriteMem(realTgAddress, trampoline_shell_code, len, NULL);
-        else
-            WriteProcessMemory(GetCurrentProcess(), realTgAddress, trampoline_shell_code, len, NULL);
+        WriteMem(realTgAddress, trampoline_shell_code, len);
 
 
         *originalFunc = originalStartSpaceAddy;
@@ -331,28 +328,103 @@ bool Hook::HookFunc(LPVOID targetFunc, LPVOID detourFunc, LPVOID* originalFunc, 
 #ifdef _DEBUG
         printf("Not enough Space, detours: %i, original: %i\n", possibleSpaces, possibleTrampolineSpaces);
 
-        if (!isPriviliged)
-        {
-            VirtualProtect((LPVOID)((uint64_t)realTgAddress - (ReadSize / 2)), ReadSize + 1, prot, &tempProt);
-        }
-
-        delete[] startInstructions;
-        delete[] foundTrampolineOffsets;
-
-        return false;
+        goto CleanUpFalse;
 #endif
     }
 
     // As the name suggests, Clean-up
 CleanUp:
 
+    HookedFuncs.insert({ targetFunc, {*originalFunc, len} });
+
     if (!isPriviliged)
-    {
         VirtualProtect((LPVOID)((uint64_t)realTgAddress - (ReadSize / 2)), ReadSize + 1, prot, &tempProt);
-    }
 
     delete[] startInstructions;
     delete[] foundTrampolineOffsets;
+
+    return true;
+
+
+CleanUpFalse:
+
+    if (!isPriviliged)
+        VirtualProtect((LPVOID)((uint64_t)realTgAddress - (ReadSize / 2)), ReadSize + 1, prot, &tempProt);
+
+    delete[] startInstructions;
+    delete[] foundTrampolineOffsets;
+
+    return false;
+}
+
+bool Hook::UnHookFunc(LPVOID targetFunc)
+{
+    HookedFuncInfo& ogFuncInfo = HookedFuncs[targetFunc];
+
+    LPVOID actualTargetFunc = GetRealFunctionAddress(targetFunc);
+
+    int ogDetourSize = ogFuncInfo.len + 5; // 5 is the size of the near jmp instruction (E9 + 4 byte Address)
+
+    BYTE* func_filler = new BYTE[max(ogDetourSize, 14)];
+    memset(func_filler, 0xCC, max(ogDetourSize, 14));
+
+    BYTE* og_firstInstructions = new BYTE[ogFuncInfo.len];
+
+    DWORD prot = 0, tempProt = 0;
+    BOOL readStatus = false;
+
+
+    // Deal with the detour to call the og func first
+    if (!isPriviliged)
+        VirtualProtect(ogFuncInfo.newOriginalFuncAddy, ogDetourSize, PAGE_EXECUTE_READWRITE, &prot);
+
+    // Copy the original instructions first
+    // We cannot continue without reading the original instructions first, because they are needed in the next step
+    if (!ReadMem(ogFuncInfo.newOriginalFuncAddy, og_firstInstructions, ogFuncInfo.len)) {
+        delete[] og_firstInstructions;
+        delete[] func_filler;
+        return false;
+    }
+
+    // Replace with 0xCC (padding)
+    WriteMem(ogFuncInfo.newOriginalFuncAddy, func_filler, ogDetourSize);
+        
+    if (!isPriviliged)
+        VirtualProtect(ogFuncInfo.newOriginalFuncAddy, ogDetourSize, prot, &tempProt);
+
+
+
+    // Deal with the original function itself (replace the jmp to the detour func with the original instructions)
+    if (!isPriviliged)
+        VirtualProtect(actualTargetFunc, ogFuncInfo.len, PAGE_EXECUTE_READWRITE, &prot);
+
+#if defined(_M_X64) || defined(__x86_64__)
+
+    int trampolineJmpOffset = 0;
+    // Read the offset from the trampoline jmp (igonre the jmp, and just read the 4 next bytes)
+    if (!ReadMem((LPVOID)((uint64_t)actualTargetFunc + 1), &trampolineJmpOffset, 4)) {
+        delete[] og_firstInstructions;
+        delete[] func_filler;
+        return false;
+    }
+
+#ifdef _DEBUG
+    printf("read offset = 0x%08X\n", trampolineJmpOffset);
+    printf("trampoline addy: 0x%p\n", (LPVOID)(trampolineJmpOffset + (uint64_t)actualTargetFunc + 5));
+#endif // _DEBUG
+
+    WriteMem((LPVOID)(trampolineJmpOffset + (uint64_t)actualTargetFunc + 5), func_filler, 14);
+
+#endif
+
+    WriteMem(actualTargetFunc, og_firstInstructions, ogFuncInfo.len);
+
+    if (!isPriviliged)
+        VirtualProtect(actualTargetFunc, ogFuncInfo.len, prot, &tempProt);
+
+
+    delete[] og_firstInstructions;
+    HookedFuncs.erase(targetFunc);
 
     return true;
 }
--- ecohook.h
+++ ecohook.h
@@ -12,6 +12,7 @@ ToDo:
 namespace Hook
 {
 	bool HookFunc(LPVOID targetFunc, LPVOID detourFunc, LPVOID* originalFunc, unsigned int len);
+	bool UnHookFunc(LPVOID targetFunc);
 
 	//typedef bool CustomWriteMemFunc(LPVOID lpAddress, LPVOID lpData, size_t size, SIZE_T* lpBytesWritten);
 	typedef bool CustomRWFunc(LPVOID, LPVOID, size_t, SIZE_T*);
